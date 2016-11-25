01f961fa66101a64b4e3087faed61d533fbd79a3
Phuc <zenithtekla@gmail.com>
2016-11-25 01:13:20 -0800

    polish

--- dev/client/app/calibrates/controllers/multer-pane.controller.js
+++ dev/client/app/calibrates/controllers/multer-pane.controller.js
@@ -1,13 +1,29 @@
 (function () {
   'use strict';
-  
+
   angular
     .module('calibrates')
     .controller('MulterPaneController', MulterPaneController);
 
-  MulterPaneController.$inject = ['$scope'];
+  MulterPaneController.$inject = ['$scope', '$http'];
 
-  function MulterPaneController($scope) {
-    console.log('hello from multer-pane');
+  function MulterPaneController($scope, $http) {
+    let vm    = this, URI = 'http://esp21:3003/multer_upload';
+    vm.title  = 'filename_placeholder';
+    vm.formData = {};
+    vm.submit = function () {
+      let config = {
+        params: { dossier: vm.formData.file},
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      };
+      console.log('File Attrs', config);
+      $http
+        // .get('http://esp21:3003/dossier_upload')
+        .post(URI, vm.formData)
+        .then(res=>console.log(res.data))
+        .catch(err=>console.error(err));
+    };
   }
 })();
---
+++ dev/client/app/calibrates/directives/file-input.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('calibrates')
+    .directive('fileInput', ['$parse', 
+      function ($parse) {
+        return {
+          restrict: 'A',
+          link: function (scope, element, attributes) {
+            element.bind('change', function () {
+              $parse(attributes.fileInput)
+                .assign(scope,element[0].files);
+              scope.$apply()
+            });
+          }
+        };
+      }
+    ]); 
+})();
