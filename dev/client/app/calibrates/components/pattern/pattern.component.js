2b119bf1fb479ebb178750888c2f62b2f40e015b
Phuc <zenithtekla@gmail.com>
2016-11-30 09:46:19 -0800

    component pattern
    ground to refactor existing set of directives to components, a feature that Angular1 has backward integration and inverted inheritance after the Angular2 success and concepts

---
+++ dev/client/app/calibrates/components/pattern/pattern.component.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('calibrates')
+    .component('componentPattern',
+
+    {
+      bindings: {
+        message: '='
+      },
+      // template: '<li ng-repeat="x in heroes">{{ x.name }} </li>',
+      template:'<div>Provide {{$ctrl.message}}</div>',
+
+      // The controller that handles our component logic
+      controller: function () {
+        this.message = "the component feature"
+      }
+  })
+})();
